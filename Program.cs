5c89cbe50631592b8c3c2163fdc5d8f05559f401
vlad <vlad.gb.99@mail.ru>
2021-04-26 14:55:20 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Protractor;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace TestareSoft4
+{
+    class Program
+    {
+        //creem un driver
+        private static IWebDriver driver;
+
+        
+        static void Main(string[] args)
+        {
+            //creem un obiec ChromeDriver - pentru google chrome
+            driver = new ChromeDriver();
+
+            //apelam functia un transmitem site-ul, elementul search, si elementul din header
+            siteSearch("https://www.google.com/", new Xpaths().googleXpath, new Xpaths().googleHeader);
+            //siteSearch("https://www.youtube.com/", new Xpaths().youtubeXpath, new Xpaths().youtubeHeader);
+            //siteSearch("https://www.amazon.com/", new Xpaths().amazonXpath, new Xpaths().amazonHeader);
+            //siteSearch("https://www.ebay.com/", new Xpaths().ebayXpath, new Xpaths().ebayHeader);
+        }
+        
+       
+        //metoda Search care primeste 3 parametri: site - xpath(ce cautam) - xpath pentru header (un element din header)
+        static void siteSearch(string siteUrl, string searchXpath, string headerXpath)
+        {
+            //marim fereastra
+            driver.Manage().Window.Maximize();
+            //accesam url-ul primit ca parametru
+            driver.Navigate().GoToUrl(siteUrl);
+            //cautam elementul unde urmeaza sa introducem cuvintul dorit
+            var search = driver.FindElement(By.XPath(searchXpath));
+            //facem click pe elementul gasit (search)
+            search.Click();
+            //introducem cuvintul
+            search.SendKeys("computer");
+            //tastam tasta enter
+            search.SendKeys(Keys.Enter);
+
+            //verificam daca exista elementul cautat in header
+            if (driver.FindElement(By.XPath(headerXpath)) != null)
+            {
+                //daca e diferit de null, atunci elementul exista, a fost gasit
+                Console.WriteLine("Header is displayed");
+                Console.ReadLine();
+            }
+            else
+            {
+                //daca e null, elementul nu a fost gasit.
+                Console.WriteLine("Header is not displayed");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Xpaths.cs
@@ -0,0 +1,20 @@
+﻿namespace TestareSoft
+{
+    public class Xpaths
+    {
+        //elementul pentru input cu name = q;
+        public string googleXpath = "//input[@name='q']";
+        //elementul care se afla in header, cautam dupa id, in cazul dat cautam o imagine
+        public string googleHeader = "//a[@id='logo']";
+        
+        public string youtubeXpath = "//input[@name='search_query']";
+        public string youtubeHeader = "//yt-icon[@id='logo-icon']";
+        
+        public string amazonXpath = "//input[@name='field-keywords']";
+        public string amazonHeader = "//a[@id='nav-logo-sprites']";
+        
+        public string ebayXpath = "//input[@name='_nkw']";
+        public string ebayHeader = "//a[@id='gh-la']";
+
+    }
+}
